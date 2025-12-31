a19317a0f2ad0a38ea99cb573c76d760ca2c4e41
Wheatley606 <42217842+Wheatley606@users.noreply.github.com>
2025-12-31 18:47:28 +0100

    Code Cleanup + HotFix for ChipCounter symbol

--- source/mods/Utils/NameObjFactoryListPatcher.hpp
+++ source/mods/Utils/NameObjFactoryListPatcher.hpp
@@ -9,21 +9,18 @@ namespace stardust::utils
 {
     inline void PatchEntryString(u64 EntryOffset, u64 StringAddress)
     {
-        exl::patch::CodePatcher cp(0xDEADCAFE); 
         u64 ObjFactoryListOffset = reinterpret_cast<uintptr_t>((void*)(((char*)&NameObjFactoryPatch::NameObjListAccessor) + EntryOffset));
         *(u64*)(ObjFactoryListOffset) = EntryOffset;
     }
 
     inline void PatchEntryCreateFunction(u64 EntryOffset, u64 CreateFunctionAddress)
     {
-        exl::patch::CodePatcher cp(0xDEADCAFE); 
         u64 ObjFactoryListOffset = reinterpret_cast<uintptr_t>((void*)(((char*)&NameObjFactoryPatch::NameObjListAccessor) + EntryOffset));
         *(u64*)(ObjFactoryListOffset + 8) = CreateFunctionAddress;
     }
 
     inline void PatchEntry(u64 EntryOffset, u64 StringAddress, u64 CreateFunctionAddress)
     {
-        exl::patch::CodePatcher cp(0xDEADCAFE); 
         u64 ObjFactoryListOffset = reinterpret_cast<uintptr_t>((void*)(((char*)&NameObjFactoryPatch::NameObjListAccessor) + EntryOffset));
         *(u64*)(ObjFactoryListOffset) = StringAddress;
         *(u64*)(ObjFactoryListOffset + 8) = CreateFunctionAddress;
